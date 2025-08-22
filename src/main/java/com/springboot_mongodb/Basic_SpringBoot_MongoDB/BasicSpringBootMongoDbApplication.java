7be6e6dca786b4f4b6d1e7bf39739546f6826d10
Jad <jad-abou-hawili@hotmail.com>
2025-08-22 21:14:24 +0300

    Start Spring Initialization

---
+++ src/main/java/com/springboot_mongodb/Basic_SpringBoot_MongoDB/BasicSpringBootMongoDbApplication.java
@@ -0,0 +1,13 @@
+package com.springboot_mongodb.Basic_SpringBoot_MongoDB;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BasicSpringBootMongoDbApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(BasicSpringBootMongoDbApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/springboot_mongodb/Basic_SpringBoot_MongoDB/BasicSpringBootMongoDbApplicationTests.java
@@ -0,0 +1,13 @@
+package com.springboot_mongodb.Basic_SpringBoot_MongoDB;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BasicSpringBootMongoDbApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
